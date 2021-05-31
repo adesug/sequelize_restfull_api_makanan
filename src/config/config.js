6e4223784d0687213971130630932f5828b93787
adesug <adesugiantoro89@gmail.com>
2021-05-31 10:20:54 +0700

    add progfile

--- src/config/config.js
+++ src/config/config.js
@@ -37,11 +37,22 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "sequelize_foods",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "sequelize_foods",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    use_env_variable: 'DATABASE_URL',
+      dialect: "postgress",
+      protocol: "postgress",
+      dialectOptions: {
+        ssl : {
+          require : true,
+          rejectUnauthorized : false
+        }
+      }
   }
 }
